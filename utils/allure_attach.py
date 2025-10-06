edc4d0ac4390544fbe52c1e98ba4bafa895eff70
Yury Kambur <ydkambur@gmail.com>
2025-10-06 22:24:25 +0300

    update attach

--- utils/allure_attach.py
+++ utils/allure_attach.py
@@ -9,7 +9,6 @@ def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.execute("getLog", {'type': 'browser'})['value'])
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
-
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
