47eadb7ab7fd2b00b2520ef0c842f6ff239e233b
Yury Kambur <ydkambur@gmail.com>
2025-10-08 00:20:46 +0300

    add one more browser

--- conftest.py
+++ conftest.py
@@ -2,13 +2,19 @@ import os
 
 import pytest
 from dotenv import load_dotenv
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selene import browser
 from utils import allure_attach
 
 DEFAULT_BROWSER_VERSION = "128.0"
+DEFAULT_BROWSER_NAME = "chrome"
 
 def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        default='chrome'
+    )
     parser.addoption(
         '--browser_version',
         default='128.0'
@@ -21,10 +27,16 @@ def load_env():
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_name = request.config.getoption('--browser_name')
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
-    options.set_capability("browserName", "chrome")
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.set_capability("browserName", "chrome")
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.set_capability("browserName", "firefox")
+    options.set_capability("browserName", browser_name)
     options.set_capability("browserVersion", browser_version)
     options.set_capability("selenoid:options", {
         "enableVNC": True,
