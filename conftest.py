e0d11701de635866ae85d353faf47b568b64a17f
Yury Kambur <ydkambur@gmail.com>
2025-10-07 23:40:10 +0300

    add env data

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.options import Options
+from selene import browser
+from utils import allure_attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "128.0")
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": True
+    })
+
+    selenoid_login = os.getenv("LOGIN")
+    selenoid_pass = os.getenv("PASSWORD")
+    selenoid_url = os.getenv("URL")
+
+    browser.config.driver_remote_url = f"https://{selenoid_login}:{selenoid_pass}{selenoid_url}"
+    browser.config.driver_options = options
+    browser.config.timeout = 10
+
+    yield browser
+
+    allure_attach.add_screenshot(browser)
+    allure_attach.add_logs(browser)
+    allure_attach.add_html(browser)
+    allure_attach.add_video(browser)
+
+    browser.quit()
+
